812542c40ba9a721117e5a7123354b008a42e8bb
tDAOminds <109101043+tDAOminds@users.noreply.github.com>
2022-07-11 22:44:33 +0200

    ico tutorial

---
+++ NFT-Collection/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9CF1c1d27e49DEd653A52A6474a9129Be30B655c";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
